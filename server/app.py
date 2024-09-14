10be438efb3208b698ebee2f63978ef2070b1796
Sandeep Kumar Panigrahi <sandeeppanigrahi3570@gmail.com>
2024-09-14 23:44:54 +0530

    Updated app.py

--- server/app.py
+++ server/app.py
@@ -21,19 +21,25 @@ def set_limit():
 
     if not user_id or not isinstance(limit, int) or not isinstance(period, int):
         return jsonify({"message": "Invalid input", "status": "error"}), 400
+    if not rate_limiter.request_user_present(user_id):
+        rate_limiter.set_limit(user_id, limit, period)
 
-    rate_limiter.set_limit(user_id, limit, period)
-    return jsonify({"message": f"Rate limit set for {user_id}", "status": "success"}), 200
+    return jsonify({"message": f"Rate limit set for user: {user_id}", "user_id": f"{user_id}", "status": "success"}), 200
 
 
 @app.route('/api/resource', methods=['GET'])
 def access_resource():
     user_id = request.args.get('user_id', 'default_user')
 
-    if rate_limiter.allow_request(user_id):
-        return jsonify({"message": "Request allowed", "status": "success"}), 200
+    if not rate_limiter.request_user_present(user_id):
+        return jsonify({"message": f"Please set a limit first for user: {user_id}", "user_id": f"{user_id}", "status": "error"}), 400
+    
+    request_allowed, request_wait_time = rate_limiter.allow_request(user_id)
+
+    if request_allowed:
+        return jsonify({"message": f"Request allowed for user: {user_id}", "user_id": f"{user_id}", "status": "success"}), 200
     else:
-        return jsonify({"message": "Rate limit exceeded", "status": "error"}), 429
+        return jsonify({"message": "Rate limit exceeded", "time_remaining": request_wait_time, "user_id": f"{user_id}", "status": "error"}), 429
 
 
 # Run the Flask app
